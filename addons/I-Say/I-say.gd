a899a9f2b1585abce0112ee845ae048bcf976cc7
Freswinn <58446076+freswinn@users.noreply.github.com>
2026-08-23 22:14:46 -0500

    Update I-say.gd

--- addons/I-Say/I-say.gd
+++ addons/I-Say/I-say.gd
@@ -4,21 +4,28 @@ extends Node
 ## [code]I.say()[/code] or [code]I.sayerr()[/code].
 
 
+## Just the categorization of the settings.
+const _prefix : String = "plugins/i_say/"
+
+
 ## Controls whether or not [method say] and [method sayerr] actually print anything.
-var say_verbose : bool = true
+var _say_active : bool = true
+
 ## Controls the amount of I.say() calls will print.
-var say_verbosity_level : SAY_VERBOSITY_LEVEL = SAY_VERBOSITY_LEVEL.all
+var _say_verbosity_level : LVL = LVL.all
 
-## Color of the header for [method say].
-const say_color : String = "cyan"
+## Color of the header for [method say]. Because [method say] is just using
+## [method @GlobalScope.print_rich], you cannot just use hexadecimal codes or other such
+## numerical representations; only color names compatible with print_rich.
+var _say_color : String = "cyan"
+
 ## The break between the header and the information in [method say] and [method sayerr].
-const say_break : String = "  ||  "
-
+var _say_break : String = "  ||  "
 
 
 ## This dictates what kinds of [method say] statements get printed in the debugger.[br]
 ## This does not affect [method sayerr] statements.
-enum SAY_VERBOSITY_LEVEL {
+enum LVL {
 	## All say() statements.
 	all=0,
 	## Only notable and serious say() statements.
@@ -27,24 +34,36 @@ enum SAY_VERBOSITY_LEVEL {
 	serious }
 
 
+
 ## Used to print information to the console; uses [method @GlobalScope.print_rich]. See
-## [member say_verbose] and [member say_verbosity_level] to set how much this method
+## [member _say_active] and [member _say_verbosity_level] to set how much this method
 ## and [method sayerr] will chatter.
 func say(info : String, verbose_level : int = 0):
-	if !say_verbose or say_verbosity_level > verbose_level: return
+	_update_vars()
+	if !_say_active or _say_verbosity_level > verbose_level: return
 	var script_filename = get_stack()[1]["source"].split("/")[-1]
 	var method_name = get_stack()[1]["function"]
 	var line_number = get_stack()[1]["line"]
 	print_rich("[color=%s][b]%s[/b], %s @ %s %s[/color] %s" % [
-		say_color, script_filename, method_name, line_number, say_break, info])
+		_say_color, script_filename, method_name, line_number, _say_break, info])
+
 
 
 ## Used to print information to the console; uses [method @GlobalScope.printerr]. See
-## [member say_verbose] to set whether this method will chatter.
+## [member _say_active] to set whether this method will chatter.
 func sayerr(info : String):
-	if !say_verbose: return
+	_update_vars()
+	if !_say_active: return
 	var script_filename = get_stack()[1]["source"].split("/")[-1]
 	var method_name = get_stack()[1]["function"]
 	var line_number = get_stack()[1]["line"]
 	printerr("%s, %s @ %s %s %s" % [
-		script_filename, method_name, line_number, say_break, info])
+		script_filename, method_name, line_number, _say_break, info])
+
+
+
+func _update_vars() -> void:
+	_say_verbosity_level = ProjectSettings.get_setting(_prefix + "verbosity_level")
+	_say_color = ProjectSettings.get_setting(_prefix + "color")
+	_say_break = ProjectSettings.get_setting(_prefix + "break")
+	_say_active = ProjectSettings.get_setting(_prefix + "active")
